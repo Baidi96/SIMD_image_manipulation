e25bee59fe064ce15cc3659d4b0ddcde4a5d2dbc
HanRuobing <ruobinghan01@126.com>
2016-12-23 11:50:00 +0800

    complete avx with little bug

--- avx_part.cpp
+++ avx_part.cpp
@@ -2,40 +2,32 @@
 #include <iostream>
 #include <cstring>
 #include <cstdio>
+#include <fstream>
 using namespace std;
 const int pic_width = 1920;
 const int pic_height = 1080;
-const int tmps = 10;
-__m256 vec_16 = _mm256_set1_ps(16);
-__m256 vec_128 = _mm256_set1_ps(128);
+__m256 vec_16 = _mm256_set1_ps(16.0);
+__m256 vec_128 = _mm256_set1_ps(128.0);
 __m256 vec_1_164 = _mm256_set1_ps(1.164383);
+void convert2rgb(__m256 &r,__m256 &g,__m256 &b,__m256 y,__m256 u,__m256 v);
+unsigned char* YUV2ARGB(unsigned char* yuv_file,int width,int height,int alpha);
+void convert2yuv(__m256 r,__m256 g,__m256 b,__m256 &y,__m256 &u,__m256 &v);
+unsigned char* ARGB2YUV(unsigned char* rgb_file,int width,int height);
 int main(int argc, char* argv[])  
 {  
-        float op1[8] = {1.1, 2.0, 3.0, 4.0,5.0,6.0,7.0,8.0};  
-        float op2[8] = {1.3, 2.0, 3.0, 4.0,5.0,6.0,7.0,8.0};  
-        float result[8];  
-      //__m256d _mm256_load_pd (double const * mem_addr)
-        __m256  a;  
-        __m256  b;  
-        __m256  c;  
-        // Load
-        a = _mm256_loadu_ps (op1) ;
-        b = _mm256_loadu_ps (op2) ;
-        // Calculate  
-        c = _mm256_mul_ps (a, b);
-        //c =_mm256_round_ps (c, (_MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC));
-        // Store  
-        _mm256_storeu_ps (result,c);
-      for(int i =0;i<8;i++)
-      	cout<<result[i]<<' ';
-      for(int i =0;i<8;i++)
-      		if(result[i]>0)result[i]=-1;
-      		else if(result[i]<0)result[i]=2;
-      	c = _mm256_loadu_ps (result) ;
-      	 _mm256_storeu_ps (result,c);
-      for(int i =0;i<8;i++)
-      	cout<<result[i]<<' ';
-        return 0;  
+	ifstream fin;
+	fin.open("dem1.yuv");
+	int char_num = (pic_height*pic_width*3)>>1;
+	unsigned char* yuv_file=new unsigned char[char_num];
+	fin.read((char*)yuv_file, char_num);
+	fin.close();
+	unsigned char* rgb_file=YUV2ARGB((unsigned char*)yuv_file,pic_width,pic_height,0);
+	yuv_file=ARGB2YUV((unsigned char*)rgb_file,pic_width,pic_height);
+	ofstream fout;
+	fout.open("trs1.yuv");
+	fout.write((char*)yuv_file,char_num);
+	fout.close();
+	return 0;
 }  
 void convert2rgb(__m256 &r,__m256 &g,__m256 &b,__m256 y,__m256 u,__m256 v)
 {
@@ -51,18 +43,17 @@ void convert2rgb(__m256 &r,__m256 &g,__m256 &b,__m256 y,__m256 u,__m256 v)
 	vec_tmp = _mm256_set1_ps(0.812968);
 	g = _mm256_sub_ps(g, _mm256_mul_ps(v,vec_tmp));
 	//round to nearest
-	r =_mm256_round_ps (r, (_MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC));
-	g =_mm256_round_ps (g, (_MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC));
-	b =_mm256_round_ps (b, (_MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC));
+	//r =_mm256_round_ps (r, (_MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC));
+	//g =_mm256_round_ps (g, (_MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC));
+	//b =_mm256_round_ps (b, (_MM_FROUND_TO_NEAREST_INT |_MM_FROUND_NO_EXC));
 	return;
 }
-//1920*1080
-char* YUV2ARGB(char* yuv_file,int width,int height,int alpha)
+unsigned char* YUV2ARGB(unsigned char* yuv_file,int width,int height,int alpha)
 {
-	char* rgb_file = new char[width*height*4];
+	unsigned char* rgb_file = new unsigned char[width*height*4];
 	__m256  y,u,v;
         	int tmp = 0;
-        	char* ystart;
+        	unsigned char* ystart;
 	for(int i = 0 ;i<height;i+=2)
 	for(int j = 0;j<width;j+=4)
 	{
@@ -86,24 +77,43 @@ char* YUV2ARGB(char* yuv_file,int width,int height,int alpha)
 	      	{
 	      		if(result[k]>255)result[k]=255;
 	      		else if(result[k]<0)result[k]=0;
-	      		rgb_file[tmp*4+1] =rgb_file[(tmp+1)*4+1] =rgb_file[(tmp+width)*4+1] =rgb_file[(tmp+width+1)*4+1] =rgb_file[(tmp+2)*4+1] =rgb_file[(tmp+2+width)*4+1] =rgb_file[(tmp+3)*4+1] =rgb_file[(tmp+3+width)*4+1] = (unsigned char)result[k];
 	      	}
-	      	
+	      	rgb_file[tmp*4+1]=result[0];
+	      	rgb_file[(tmp+1)*4+1]=result[1];
+	      	rgb_file[(tmp+width)*4+1]=result[2];
+	      	rgb_file[(tmp+width+1)*4+1]=result[3];
+	      	rgb_file[(tmp+2)*4+1]=result[4];
+	      	rgb_file[(tmp+2+width)*4+1]=result[5];
+	      	rgb_file[(tmp+3)*4+1]=result[6];
+	      	rgb_file[(tmp+3+width)*4+1]=result[7];
 	      	_mm256_storeu_ps (result,g);
 	      	for(int k =0;k<8;k++)
 	      	{
 	      		if(result[k]>255)result[k]=255;
 	      		else if(result[k]<0)result[k]=0;
-	      		rgb_file[tmp*4+2] =rgb_file[(tmp+1)*4+2] =rgb_file[(tmp+width)*4+2] =rgb_file[(tmp+width+1)*4+2] =rgb_file[(tmp+2)*4+2] =rgb_file[(tmp+2+width)*4+2] =rgb_file[(tmp+3)*4+2] =rgb_file[(tmp+3+width)*4+2] = (unsigned char)result[k];
 	      	}
-
+	      	rgb_file[tmp*4+2]=result[0];
+	      	rgb_file[(tmp+width)*4+2]=result[1];
+	      	rgb_file[(tmp+1)*4+2]=result[2];
+	      	rgb_file[(tmp+1+width)*4+2]=result[3];
+	      	rgb_file[(tmp+2)*4+2]=result[4];
+	      	rgb_file[(tmp+2+width)*4+2]=result[5];
+	      	rgb_file[(tmp+3)*4+2]=result[6];
+	      	rgb_file[(tmp+3+width)*4+2]=result[7];
 	      	_mm256_storeu_ps (result,b);
 	      	for(int k =0;k<8;k++)
 	      	{
 	      		if(result[k]>255)result[k]=255;
 	      		else if(result[k]<0)result[k]=0;
-	      		rgb_file[tmp*4+3] =rgb_file[(tmp+1)*4+3] =rgb_file[(tmp+width)*4+3] =rgb_file[(tmp+width+1)*4+3] =rgb_file[(tmp+2)*4+3] =rgb_file[(tmp+2+width)*4+3] =rgb_file[(tmp+3)*4+3] =rgb_file[(tmp+3+width)*4+3] = (unsigned char)result[k];
 	      	}
+	      	rgb_file[tmp*4+3]=result[0];
+	      	rgb_file[(tmp+width)*4+3]=result[1];
+	      	rgb_file[(tmp+1)*4+3]=result[2];
+	      	rgb_file[(tmp+1+width)*4+3]=result[3];
+	      	rgb_file[(tmp+2)*4+3]=result[4];
+	      	rgb_file[(tmp+2+width)*4+3]=result[5];
+	      	rgb_file[(tmp+3)*4+3]=result[6];
+	      	rgb_file[(tmp+3+width)*4+3]=result[7];
 		tmp+=2;
 	}
 	return rgb_file;
@@ -117,30 +127,31 @@ void convert2yuv(__m256 r,__m256 g,__m256 b,__m256 &y,__m256 &u,__m256 &v)
 	y = _mm256_add_ps(y,_mm256_mul_ps(tmp,g));
 	tmp = _mm256_set1_ps(0.097906);
 	y = _mm256_add_ps(y,_mm256_mul_ps(tmp,b));
-	y = _mm256_add_ps(y,_mm256_set1_ps(16));
+	y = _mm256_add_ps(y,_mm256_set1_ps(16.0));
 
 	u=_mm256_setzero_ps();
 	tmp=_mm256_set1_ps(0.439216);
 	u=_mm256_add_ps(u,_mm256_mul_ps(tmp,b));
-	u=_mm256_add_ps(u,_mm256_set1_ps(128));
+	u=_mm256_add_ps(u,_mm256_set1_ps(128.0));
 	u=_mm256_sub_ps(u,_mm256_mul_ps(g,_mm256_set1_ps(0.290993)));
 	u=_mm256_sub_ps(u,_mm256_mul_ps(r,_mm256_set1_ps(0.148223)));
+
 	v=_mm256_mul_ps(r, _mm256_set1_ps(0.439216));
 	v=_mm256_sub_ps(v,_mm256_mul_ps(g,_mm256_set1_ps(0.367788)));
 	v=_mm256_sub_ps(v,_mm256_mul_ps(b,_mm256_set1_ps(0.071427)));
-	v=_mm256_add_ps(v,_mm256_set1_ps(128));
+	v=_mm256_add_ps(v,_mm256_set1_ps(128.0));
 }
-char* ARGB2YUV(char* rgb_file,int width,int height)
+unsigned char* ARGB2YUV(unsigned char* rgb_file,int width,int height)
 {
-	char* yuv_file = new char[width*height*3];
+	unsigned char* yuv_file = new unsigned char[width*height*3];
         	__m256  r,g,b;
         	int tmp = 0;
-        	char* ystart;
-        	char* ustart = yuv_file+pic_height*pic_width;
+        	unsigned char* ystart;
+        	unsigned char* ustart = yuv_file+pic_height*pic_width;
 	for(int i = 0 ;i<pic_height;i+=2)
 	for(int j = 0;j<pic_width;j+=4)
 	{
-		ystart = yuv_file+i*width;
+		ystart = yuv_file+i*width+j;
 		r = _mm256_set_ps((float)rgb_file[tmp*4+1],(float)rgb_file[(tmp+1)*4+1],(float)rgb_file[(tmp+width)*4+1],(float)rgb_file[(tmp+width+1)*4+1],(float)rgb_file[(tmp+2)*4+1],(float)rgb_file[(tmp+2+width)*4+1],(float)rgb_file[(tmp+3)*4+1],(float)rgb_file[(tmp+3+width)*4+1]);
 		g = _mm256_set_ps((float)rgb_file[tmp*4+2],(float)rgb_file[(tmp+1)*4+2],(float)rgb_file[(tmp+width)*4+2],(float)rgb_file[(tmp+width+1)*4+2],(float)rgb_file[(tmp+2)*4+2],(float)rgb_file[(tmp+2+width)*4+2],(float)rgb_file[(tmp+3)*4+2],(float)rgb_file[(tmp+3+width)*4+2]);
 		b = _mm256_set_ps((float)rgb_file[tmp*4+3],(float)rgb_file[(tmp+1)*4+3],(float)rgb_file[(tmp+width)*4+3],(float)rgb_file[(tmp+width+1)*4+3],(float)rgb_file[(tmp+2)*4+3],(float)rgb_file[(tmp+2+width)*4+3],(float)rgb_file[(tmp+3)*4+3],(float)rgb_file[(tmp+3+width)*4+3]);
@@ -149,30 +160,25 @@ char* ARGB2YUV(char* rgb_file,int width,int height)
         		//store the res
         		float result[8];
 		 _mm256_storeu_ps (result,y);
-		 ystart[0]=(char)result[0];
-		 ystart[1]=(char)result[1];
-		 ystart[width]=(char)result[2];
-		 ystart[1+width]=(char)result[3];
-		 ystart[2]=(char)result[4];
-		 ystart[3]=(char)result[5];
-		 ystart[2+width]=(char)result[6];
-		 ystart[3+width]=(char)result[7];
+		 ystart[0]=(unsigned char)result[0];
+		 ystart[1]=(unsigned char)result[1];
+		 ystart[width]=(unsigned char)result[2];
+		 ystart[1+width]=(unsigned char)result[3];
+		 ystart[2]=(unsigned char)result[4];
+		 ystart[3]=(unsigned char)result[5];
+		 ystart[2+width]=(unsigned char)result[6];
+		 ystart[3+width]=(unsigned char)result[7];
 
 		 _mm256_storeu_ps (result,u);
-		 cout<<"all u result"<<endl;
-		 for(int k=0;k<8;k++)
-		 	cout<<(char)result[k]<<endl;
-		 ustart[0]=(char)result[0];
-		 ustart[1]=(char)result[4];
+		 ustart[0]=(unsigned char)result[0];
+		 ustart[1]=(unsigned char)result[4];
 
-		 char* vstart = ustart+(width*height)/4;
+		 unsigned char* vstart = ustart+(width*height)/4;
 		 _mm256_storeu_ps (result,v);
-		 cout<<"all v result"<<endl;
-		 for(int k=0;k<8;k++)
-		 	cout<<(char)result[k]<<endl;
-		 vstart[0]=(char)result[0];
-		 vstart[1]=(char)result[4];
+		 vstart[0]=(unsigned char)result[0];
+		 vstart[1]=(unsigned char)result[4];
 		tmp+=2;
 		ustart+=2;
 	}
+	return yuv_file;
 }
